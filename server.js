996fb01dbe767cc5ba321a983c630eab6c0a8a4a
Stephen Byrne <stephenbyrne@Stephens-MacBook-Pro.local>
2021-06-24 22:56:44 +0100

    sdk added

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+require('dotenv').config();
+
+
+// init server
+const app = express();
+
+
+// routes ?
+
+
+// middlewares
+
+
+// middlewares
+app.listen(process.env.PORT, () => {
+    
+    
+    // Insert Poller
+      
+        // Poll the four things
+
+})
