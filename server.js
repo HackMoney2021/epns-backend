7929269a622fdf63553ca6128b893ffae1bb09b9
John Sinclair <sinclairjohn72@gmail.com>
2021-06-25 18:18:38 +0100

    started work

--- server.js
+++ server.js
@@ -3,13 +3,37 @@ require("dotenv").config();
 
 // init server
 const app = express();
-//test
 
-// routes ?
+const infuraSettings = {
+  projectID: process.env.INFURA_PROJECT_ID,
+  projectSecret: process.env.INFURA_PROJECT_SECRET,
+};
 
-// middlewares
+const settings = {
+  infura: infuraSettings,
+};
 
-// middlewares
+const epnsSettings = {
+  network: "ropsten",
+  contractAddress: "0xc882dA9660d29c084345083922F8a9292E58787D",
+  contractABI: require("./epns_contract.json"),
+};
+
+const sdk = new epnsHelper(
+  "ropsten",
+  process.env.STREAM_PRIVATE_KEY,
+  settings,
+  epnsSettings
+);
+
+const getSubs = async () => {
+  const users = await sdk.getSubscribedUsers();
+  console.log(users);
+};
+
+console.log("call getSubs");
+getSubs();
+
 app.listen(process.env.PORT, () => {
   // Insert Poller
   // Poll the four things
