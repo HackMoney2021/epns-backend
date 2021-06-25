9af94196c4badc15b7b49b11c3f228df805a5c5f
John Sinclair <sinclairjohn72@gmail.com>
2021-06-25 18:08:48 +0100

    test

--- server.js
+++ server.js
@@ -1,23 +1,16 @@
-const express = require('express');
-require('dotenv').config();
-
+const express = require("express");
+require("dotenv").config();
 
 // init server
 const app = express();
-
+//test
 
 // routes ?
 
-
 // middlewares
-
 
 // middlewares
 app.listen(process.env.PORT, () => {
-    
-    
-    // Insert Poller
-      
-        // Poll the four things
-
-})
+  // Insert Poller
+  // Poll the four things
+});
